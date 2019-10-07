821e32d3dedbc40115557a4899b20a75d86d2c38
Damien Ostler <damienostler1@outlook.com>
2019-10-07 10:14:02 -0400

    Added factorial calculator validation

--- Damien_Portfolio/Scripts/Challenges/FactorialCalculator.js
+++ Damien_Portfolio/Scripts/Challenges/FactorialCalculator.js
@@ -4,7 +4,20 @@ const exerciseTwoOutputOne = document.getElementById("exerciseTwoOutputOne");
 const exerciseTwoOutputTwo = document.getElementById("exerciseTwoOutputTwo");
 document.getElementById("exerciseTwoCalculate").addEventListener("click", exerciseTwo)
 function exerciseTwo() {
-    let startingNumber = exerciseTwoInput.value; // get the input
+    let startingNumber = parseInt(exerciseTwoInput.value); // get the input
+    
+    if (startingNumber >= 170 || startingNumber<=0) {
+        $("#exerciseTwo").modal("hide")
+        bootbox.alert({
+            size: "small",
+            title: "Invalid Input",
+            message: "Your input needs to be less then or equal to 170 and greater than 0!",
+            callback: function () {
+                $("#exerciseTwo").modal("show")
+            }
+        })
+        return;
+    }
     let total = 1 // set the starting total to 1 so when you mutiply it by the first number it is just the first number.
     let string = "" // set a empty string to display the math leading up to the solution
     for (i = startingNumber; i > 0; i--) { // Loop down from the starting number
